fe67359409741446ab2d48be0d58d14c4fdc56ad
2b11.1902257yeosheenhern <2b11.1902257yeosheenhern@gmail.com>
2020-12-01 20:34:46 +0800

    Initialise heroku deployment

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 import joblib
 from flask_sqlalchemy import SQLAlchemy
+from flask_heroku import Heroku
 
 app = Flask(__name__)
 
 app.config.from_pyfile('config.cfg')
+heroku = Heroku(app)
+
 db = SQLAlchemy(app)
 
 joblib_file = "./application/static/model.pkl"
